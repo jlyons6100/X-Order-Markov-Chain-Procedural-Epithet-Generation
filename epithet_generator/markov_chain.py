8edc9a7ac19e3def3123485a9993174a78a0cf01
James Lyons <jlyons61@alumni.stanford.edu>
2021-12-18 21:32:37 -0800

    [markov chain] Create chain class (#16)
    * Write addWord, generateWord, getProbability methods for markov chain
    
    * Update markov chain methods, add unit tests
    
    * Add some detail in epithet generator methods
    
    * Update markov chain test
    
    * Delete old hello world test
    
    * Linting fix
    
    Co-authored-by: James Lyons <jameslyons@Jamess-MacBook-Pro.local>

--- epithet_generator/markov_chain.py
+++ epithet_generator/markov_chain.py
@@ -1,12 +1,45 @@
+import random
+
+
 class markovChain:
     def __init__(self):
-        pass
+        self.root = {}
 
-    def addWord(self, word):
-        pass
+    def add(self, word):
+        node = self.root
+        # Populate tree with letter counts
+        for letter in word.lower():
+            lettersAndCount = node.setdefault(
+                letter,
+                {"nextLetters": {}, "count": 0})
+            node = lettersAndCount["nextLetters"]
+            lettersAndCount["count"] += 1
 
-    def generateWord(self):
-        return ""
+    def generate(self, start=None):
+        wordList = []
+        node = self.root
+        # Restart chain logic
+        if start:
+            node = node[start]["nextLetters"]
+            wordList.append(start)
 
-    def getProbabilities(self):
-        pass
+        while len(node.keys()) != 0:
+            weights = [node[key]["count"] for key in node.keys()]
+            # Pick random character based on weight
+            newChar = random.choices(list(node.keys()), weights)[0]
+            wordList.append(newChar)
+            # Move to next node
+            node = node[newChar]["nextLetters"]
+        return "".join(wordList)
+
+    def getProbability(self, prefix):
+        node = self.root
+        probability = 1
+        for letter in prefix.lower():
+            if letter not in node:
+                return 0
+            total = sum([node[key]["count"] for key in node.keys()])
+            # Independent events, multiply together for probability
+            probability *= ((node[letter]["count"])/(total))
+            node = node[letter]["nextLetters"]
+        return probability
--- test/markov_chain_test.py
+++ test/markov_chain_test.py
@@ -2,4 +2,26 @@ from epithet_generator.markov_chain import markovChain
 
 
 def test_empty_generation():
-    assert markovChain().generateWord() == ""
+    assert markovChain().generate() == ""
+
+
+def test_get_probability():
+    chain = markovChain()
+    chain.add("Sea")
+    chain.add("see")
+    assert chain.getProbability("se") == 1
+    assert chain.getProbability("sea") == 0.5
+
+
+def test_generation_not_empty():
+    chain = markovChain()
+    chain.add("sea")
+    chain.add("sea")
+    assert chain.generate() == "sea"
+
+
+def test_restart_generation():
+    chain = markovChain()
+    chain.add("sea")
+    chain.add("sea")
+    assert chain.generate("s") == "sea"
